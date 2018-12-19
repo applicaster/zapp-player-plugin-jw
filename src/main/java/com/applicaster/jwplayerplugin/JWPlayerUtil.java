0b7f5a94a46d09ca2fb74b09e9906979eaa0d6c4
Dror Gdalia <dror@digic.tech>
2018-12-19 12:49:37 +0200

    added applicaster sds (midroll + postroll) support

--- src/main/java/com/applicaster/jwplayerplugin/JWPlayerUtil.java
+++ src/main/java/com/applicaster/jwplayerplugin/JWPlayerUtil.java
@@ -2,6 +2,7 @@ package com.applicaster.jwplayerplugin;
 
 import com.applicaster.player.VideoAdsUtil;
 import com.applicaster.plugin_manager.playersmanager.Playable;
+import com.applicaster.util.AppData;
 import com.longtailvideo.jwplayer.media.ads.AdBreak;
 import com.longtailvideo.jwplayer.media.ads.AdSource;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
@@ -11,8 +12,11 @@ import java.util.List;
 
 import javax.annotation.CheckReturnValue;
 
+import static com.applicaster.player.VideoAdsUtil.MIDROLL_INTERVAL;
+
 @CheckReturnValue
 public class JWPlayerUtil {
+    final static String EXTERNAL_PLAYER_MIDROLL_INTERVAL="midroll_interval_external_player";
 
     public static PlaylistItem getPlaylistItem(Playable playable){
         PlaylistItem result=null;
@@ -36,11 +40,36 @@ public class JWPlayerUtil {
         // Create your ad schedule
         List<AdBreak> adSchedule = new ArrayList<>();
 
-        String imaAdUnit = VideoAdsUtil.getAccountPreroll(playable.isLive(), false);
-        AdBreak adBreak = new AdBreak("pre", AdSource.IMA, imaAdUnit); // "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=");
-
+        String imaPrerollAdUnit = VideoAdsUtil.getAccountPreroll(playable.isLive(), false);
+        AdBreak adBreak = new AdBreak("pre", AdSource.IMA, imaPrerollAdUnit); // "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=");
         adSchedule.add(adBreak);
 
+        String imaPostrollAdUnit = VideoAdsUtil.getAccountPostroll();
+        AdBreak postrollAdBreak = new AdBreak("post", AdSource.IMA, imaPostrollAdUnit );
+        adSchedule.add(postrollAdBreak);
+
+        String imaMidrollAdUnit = VideoAdsUtil.getAccountMidroll();
+        int breakInterval = getMidrollInterval(); //percentage
+        if (breakInterval> 0) {
+            for (int i=1; breakInterval*i<100; i++ ){
+                AdBreak midrollAdBreak = new AdBreak(breakInterval*i + "%", AdSource.IMA, imaMidrollAdUnit);
+                adSchedule.add(midrollAdBreak);
+            }
+        }
+
         return adSchedule;
+    }
+
+    private static int getMidrollInterval(){
+        int interval  = 0;
+        String extensionValue = (String) AppData.getAPExtension(EXTERNAL_PLAYER_MIDROLL_INTERVAL);
+        if(extensionValue != null) {
+            try {
+                interval = (Integer.parseInt((String) AppData.getAPExtension(MIDROLL_INTERVAL)));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return interval;
     }
 }
